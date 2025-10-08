0a73628ba8a0dc3f198a790652b7b6c543585766
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-10-08 20:20:50 -0300

    Create A0008-type-never.ts

---
+++ src/A0008-type-never/A0008-type-never.ts
@@ -0,0 +1,6 @@
+// E um tipo de uma funcao ou metodo que Nunca ira retornar nada, por exemplo um loop infinito ou um erro
+export function criaErro(): never {
+    throw new Error('Erro qualquer');
+}
+
+criaErro()
