03ed31b78d2c5b4aac3f45e4d4f7352ce5526bd7
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-01 22:13:44 -0300

    add coment

--- src/A0046-generics-4/A0046-generics-4.ts
+++ src/A0046-generics-4/A0046-generics-4.ts
@@ -1,6 +1,11 @@
 type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
 
+//O → tipo do objeto.
+//K → tipo da chave, que deve ser uma das chaves de O (keyof O).
+
 const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
+
+//Como o TypeScript já sabe os tipos pelo ObterChaveFn, ele entende automaticamente: que chave só pode ser uma chave válida do objeto e que o retorno é exatamente o valor do tipo daquela chave
 
 const animal = {
     cor: 'Rosa',
