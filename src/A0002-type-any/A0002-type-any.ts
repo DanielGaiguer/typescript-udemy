506fcfa3b3fb81e0f6287a866d90cf48997379f7
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-03 10:33:52 -0300

    Update A0002-type-any.ts

--- src/A0002-type-any/A0002-type-any.ts
+++ src/A0002-type-any/A0002-type-any.ts
@@ -1,7 +1,9 @@
 //E uma falta de tipo ou qualquer tipo
 //Utilize any apenas em ultimo caso
 function showMassage(msg: any) {
-  return msg;
+    return msg;
 }
 
 console.log(showMassage([1, 2, 3]));
+console.log(showMassage('Olá'));
+console.log(showMassage(1));
