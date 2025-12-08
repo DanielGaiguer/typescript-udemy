af104c33d08a29531f5462a180d9c53c1475e93e
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-08 10:46:37 -0300

    Create A0049-generics-7.ts

---
+++ src/A0049-generics-7/A0049-generics-7.ts
@@ -0,0 +1,23 @@
+// Nos principios Solid, existem funcoes e classes bem pequenas, uma delas, sao as funcoes para a checagem de tipos.
+export function isNumber(value: unknown): value is number {
+    return typeof value === 'number'; //Isso é um Type Guard
+}
+
+console.log(isNumber('123'));
+console.log(isNumber(123));
+
+export function soma<T>(...args: T[]): number | null {
+    const retorno = args.reduce((sum, value) => {
+        if (isNumber(sum) && isNumber(value)) {
+            return sum + value;
+        }
+        return sum;
+    }, 0);
+
+    return retorno;
+}
+
+console.log(soma(1, 2, 3));
+// console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1])); // Desta forma nao e mais possivel nas versoes atuais
+console.log(soma<number | string>(1, 2, 3, 'a', 'b', 'c', 1));
+console.log(soma('a', 'b', 'c'));
